bb32a16bb5150aec2ccd75a0bbcb22949d4a1a4e
Amna <110226508+Amna-ismail@users.noreply.github.com>
2024-08-11 00:46:09 +0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace HCI_Lab_Poject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.startButton = new System.Windows.Forms.Button();
+            this.snapButton = new System.Windows.Forms.Button();
+            this.txtScore = new System.Windows.Forms.Label();
+            this.txtHighScore = new System.Windows.Forms.Label();
+            this.picCanvas = new System.Windows.Forms.PictureBox();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.picCanvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Image = ((System.Drawing.Image)(resources.GetObject("startButton.Image")));
+            this.startButton.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.startButton.Location = new System.Drawing.Point(594, 45);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(167, 86);
+            this.startButton.TabIndex = 0;
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.StartGame);
+            // 
+            // snapButton
+            // 
+            this.snapButton.Image = ((System.Drawing.Image)(resources.GetObject("snapButton.Image")));
+            this.snapButton.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.snapButton.Location = new System.Drawing.Point(594, 137);
+            this.snapButton.Name = "snapButton";
+            this.snapButton.Size = new System.Drawing.Size(167, 85);
+            this.snapButton.TabIndex = 1;
+            this.snapButton.UseVisualStyleBackColor = true;
+            this.snapButton.Click += new System.EventHandler(this.TakeSnapShot);
+            // 
+            // txtScore
+            // 
+            this.txtScore.AutoSize = true;
+            this.txtScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtScore.Location = new System.Drawing.Point(633, 239);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(88, 24);
+            this.txtScore.TabIndex = 2;
+            this.txtScore.Text = "Score: 0";
+            // 
+            // txtHighScore
+            // 
+            this.txtHighScore.AutoSize = true;
+            this.txtHighScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHighScore.Location = new System.Drawing.Point(621, 278);
+            this.txtHighScore.Name = "txtHighScore";
+            this.txtHighScore.Size = new System.Drawing.Size(139, 24);
+            this.txtHighScore.TabIndex = 3;
+            this.txtHighScore.Text = "High Score:   ";
+            // 
+            // picCanvas
+            // 
+            this.picCanvas.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.picCanvas.Image = global::HCI_Lab_Poject.Properties.Resources.laiba2_01;
+            this.picCanvas.Location = new System.Drawing.Point(31, 3);
+            this.picCanvas.Name = "picCanvas";
+            this.picCanvas.Size = new System.Drawing.Size(500, 411);
+            this.picCanvas.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picCanvas.TabIndex = 4;
+            this.picCanvas.TabStop = false;
+            this.picCanvas.Paint += new System.Windows.Forms.PaintEventHandler(this.UpdatePictureBoxGraphics);
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 150;
+            this.gameTimer.Tick += new System.EventHandler(this.GameTimerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(839, 417);
+            this.Controls.Add(this.picCanvas);
+            this.Controls.Add(this.txtHighScore);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.snapButton);
+            this.Controls.Add(this.startButton);
+            this.Name = "Form1";
+            this.Text = "Snake Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.picCanvas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button snapButton;
+        private System.Windows.Forms.Label txtScore;
+        private System.Windows.Forms.Label txtHighScore;
+        private System.Windows.Forms.PictureBox picCanvas;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
+
